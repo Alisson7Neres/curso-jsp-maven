c3fb26975619e31a57cd5ba06d352c6999b03ff0
Alisson7Neres <alisson.neres@hotmail.com>
2022-03-07 10:29:18 -0300

    Implementado logout e carregando usuário logado

--- src/main/java/com/jsp/servlets/ServletLogin.java
+++ src/main/java/com/jsp/servlets/ServletLogin.java
@@ -27,7 +27,17 @@ public class ServletLogin extends HttpServlet {
 	// Recebe os dados peça URL em parametros
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doPost(request, response);
+		
+		String acao = request.getParameter("acao");
+		
+		if(acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			request.getSession().invalidate();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			redirecionar.forward(request, response);
+		} else {
+			doPost(request, response);
+		}
+		
 	}
 
 	// Recebe os dados enviados por um formulario
